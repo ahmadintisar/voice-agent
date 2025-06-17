53ccb4b2684619e3f6065579ea0aede75dd49953
Ahmad Intisar <ahmadintisar@Ahmads-MacBook-Pro.local>
2025-06-17 21:56:36 +0500

    Fixed system path for render

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(project_root, "src"))
 
-from src.voice_assistant import VoiceAssistant
+from voice_assistant import VoiceAssistant
 import threading
 import queue
 
